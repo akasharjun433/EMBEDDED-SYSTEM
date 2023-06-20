35da3fefe1cd995846f215ce45fe5466f26b3303
akasharjun433 <115264046+akasharjun433@users.noreply.github.com>
2023-06-20 17:39:14 +0530

    Add files via upload

---
+++ And_gate.c
@@ -0,0 +1,30 @@
+#include<AT89S52.h>
+
+#define LED P2_0
+#define SW1 P3_0
+#define SW2 P3_2
+#define ON 0
+#define OFF 1
+#define INPUT 1
+
+void main()
+{
+__bit sw1,sw2;
+LED=OFF;
+SW1=INPUT;
+SW2=INPUT;
+while(1)
+{
+sw1=SW1;
+sw2=SW2;
+
+if(sw1==ON && sw2==ON)
+{
+LED=ON;
+}
+else
+{
+LED=OFF;
+}
+}
+}
